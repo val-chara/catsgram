56ce5852a8536015417bee8978bcfdffd18279fa
valchara <charyevalery@mail.ru>
2026-06-08 17:55:12 +0300

    Созданы  Windows PowerShell

---
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    private final Map<Long, User> users = new HashMap<>();
+    private final Map<String, Long> emailToIdMap = new HashMap<>();
+
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    public User create(User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Имейл должен быть указан");
+        }
+        if (emailToIdMap.containsKey(user.getEmail())) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        emailToIdMap.put(user.getEmail(), user.getId());
+        return user;
+    }
+
+    public User update(User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        User existingUser = users.get(newUser.getId());
+        if (existingUser == null) {
+            throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+        }
+        if (newUser.getEmail() != null && !newUser.getEmail().equals(existingUser.getEmail())) {
+            if (emailToIdMap.containsKey(newUser.getEmail())) {
+                throw new DuplicatedDataException("Этот имейл уже используется");
+            }
+            emailToIdMap.remove(existingUser.getEmail());
+            emailToIdMap.put(newUser.getEmail(), newUser.getId());
+            existingUser.setEmail(newUser.getEmail());
+        }
+        if (newUser.getUsername() != null) {
+            existingUser.setUsername(newUser.getUsername());
+        }
+        if (newUser.getPassword() != null) {
+            existingUser.setPassword(newUser.getPassword());
+        }
+        return existingUser;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
